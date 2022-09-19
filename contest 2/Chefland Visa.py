f0295399a9ff74ed7b95f619dae46af2ad08ab33
Naman Sehwal <41113007+namansehwal@users.noreply.github.com>
2022-09-19 19:50:24 +0530

    Update Chefland Visa.py

--- contest 2/Chefland Visa.py
+++ contest 2/Chefland Visa.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 TestCases = int(input())
 
 for _ in range(TestCases):
@@ -11,16 +10,3 @@ for _ in range(TestCases):
         print('NO')
        
 
-=======
-TestCases = int(input())
-
-for _ in range(TestCases):
-    x1,x2,y1,y2,z1,z2 = [int(x) for x in input().split()]
-    
-    #conditions
-    if (20<=x1<=50 and 20<=x2<=50) and (1900<=y1<=2100 and  1900<=y2<=2100) and (1<=z1<=6 and  1<=z2<=6):
-               print('YES')
-    else:
-        print('NO')
-       
->>>>>>> cbb9e77978142516ce8fa6ef34622da657cb3d36
